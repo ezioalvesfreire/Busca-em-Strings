bcc9997c3d517949a833499c278c02501ebf7fe2
Ézio Alves <ezio.freire@catolicasc.org.br>
2020-04-25 01:52:12 -0300

    Primeiro commit

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    printf("\n");
+    printf("================ Estrutura de dados Busca-em-Strings ========================");
+    printf("\n\n\n");
+
+
+    return 0;
+}

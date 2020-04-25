35a9d26606bad9d55b998b1cd6a5d21cf9ca8f87
Ézio Alves <ezio.freire@catolicasc.org.br>
2020-04-25 03:31:26 -0300

    implementado a função busca em Strings

--- main.cpp
+++ main.cpp
@@ -1,14 +1,60 @@
 #include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
 #include <iostream>
 
 using namespace std;
 
+
+bool existe( char palavra[], char frase[]){
+    int teste[strlen(palavra)];
+    int aux;
+
+     if(strlen(palavra) > strlen(frase ) ){
+        return false;
+     }
+     if(strcmp(palavra, frase) ==0 ){
+        return true;
+     }
+     for(int i = 0; i < strlen(frase) - strlen(palavra); i++){
+        if(frase[i] == palavra[0]){
+
+            for(int k=0; k<strlen(palavra); k++)
+                teste[k]=0;
+
+            aux=i;
+            for(int j=0; j<strlen(palavra); j++){
+                if(palavra[j]= frase[aux]){
+                    teste[j] = 1;
+                    aux++;
+                }
+            }
+            if( testar(teste, strlen(palavra))){
+                return true;
+            }
+        }
+
+     }
+     return false;
+}
 int main()
 {
     printf("\n");
     printf("================ Estrutura de dados Busca-em-Strings ========================");
     printf("\n\n\n");
 
+    char palavra[] = "strings";
+    char frase[] = "teste busca em strings";
 
+    if(existe(palavra, frase)){
+        printf("\n\n");
+        printf("A palavra existe");
+    }else{
+            printf("\n\n\n");
+            printf("A palavra não existe");
+    }
+
+    printf("\n\n\n");
+    system("pause");
     return 0;
 }
